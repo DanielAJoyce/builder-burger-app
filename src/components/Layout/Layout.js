da24a12385c68f009dc3f2dceab253b3fff6c406
Daniel Joyce <Naiyon@users.noreply.github.com>
2018-03-11 17:22:59 +0000

    Lesson 134 Complete: Pre-burger menu creation
    Will try next one myself.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class layout extends Component{
 
-const layout = (props) => {
+  state = {
+    showSideDrawer: true
+  }
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer:false});
+  }
 
-  return(
-    <Aux>
-      <div>
-        <Toolbar/>
-      </div>
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </Aux>
-  )
+  render(){
+    return(
+      <Aux>
+        <div>
+          <Toolbar/>
+          <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        </div>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
 }
+
 
 
 export default layout;
